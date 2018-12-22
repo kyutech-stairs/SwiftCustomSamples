0c97fd1340eb396992885b5021a43aac6487d099
KO-YAMAKO <ko.foot1103@gmail.com>
2018-12-22 22:18:20 +0900

    update ViewController storyboard

--- SwiftCustomSample/SwiftCustomSample/FirstViewController/FirstViewController.swift
+++ SwiftCustomSample/SwiftCustomSample/FirstViewController/FirstViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class FirstViewController: UIViewController {
     
     @IBAction func secondSegueAction(_ sender: Any) {
-        let second = SecondViewController.instantiate()
+
+        // あらかじめUIViewControllerと同じ名前のStroyboardを用意しておくと、instantiate()クラスを呼び出した時に自動的に
+        // 同じ名前のStoryboardにアクセスしてViewを生成してくれる。
+        // ちなみにフォルダ分けもUIViewControllerごとがオススメ
+        let second = SecondViewController.instantiate() // extensionでStoryboardInstantiableを利用していると使える
         self.present(second, animated: true, completion: nil)
     }
 }
